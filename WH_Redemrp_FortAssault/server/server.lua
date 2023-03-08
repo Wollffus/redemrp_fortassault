e627c32e3603a7fe40905a920ab0eff1ef989bfb
Wollffus <58875783+Wollffus@users.noreply.github.com>
2023-03-08 13:16:10 -0800

    Update server.lua

--- WH_Redemrp_FortAssault/server/server.lua
+++ WH_Redemrp_FortAssault/server/server.lua
@@ -22,6 +22,16 @@ AddEventHandler("WH_Redemrp_FortAssault:startToRob", function()
     end
 end)
 
+Citizen.CreateThread(function()
+	if GetCurrentResourceName() ~= "WH_Redemrp_FortAssault" then -- There will be problems with the script if the name is changed...
+		print("^1=====================================")
+		print("^1SCRIPT NAME OTHER THAN ORIGINAL")
+		print("^1THERE WILL/MAY BE PROBLEMS WITH THE SCRIPT IF THE NAME IS CHANGED")
+		print("^1CHANGE NAME TO: ^2WH_Redemrp_FortAssault^1")
+		print("^1=====================================^0")
+	end
+end)
+
 RegisterNetEvent("WH_Redemrp_FortAssault:payout")
 AddEventHandler("WH_Redemrp_FortAssault:payout", function()
     local _source = source
@@ -42,52 +52,3 @@ AddEventHandler("WH_Redemrp_FortAssault:payout", function()
             ItemData.AddItem(wollffus)
         user.addMoney(Config.RewardMoney)
 end)
------------------------------------------------------------------------------VERSION CHECK--------------------------------------------------------------------------------------
-Citizen.CreateThread(function()
-    versionchecker()
-end)
-
-function versionchecker()
-    local data = getdatafromapi("https://raw.githubusercontent.com/Wollffus/filevalidation/main/WH_Redemrp_FortAssault.json", function(data)
-        if data then
-            local dataversion = data.version
-            local change = data.changelog
-            if version < dataversion then
-                print("A new update is available!","\nYour version : " ..version,"\nNew version :  "..dataversion ,"\nChange log : \n".. change)
-            end
-        end
-    end)
-end
-
-function getdatafromapi(url,cb)
-    local data = nil
-    PerformHttpRequest(url, function(code, result, headers)
-        if result and #result then
-            data = json.decode(result)
-            cb(data)
-        end
-    end, "GET")
-end
------------------------------------------------------------------------------VERSION CHECK--------------------------------------------------------------------------------------
-
-Citizen.CreateThread(function()
-	if GetCurrentResourceName() ~= "WH_Redemrp_FortAssault" then -- There will be problems with the script if the name is changed...
-		print("^1=====================================")
-		print("^1SCRIPT NAME OTHER THAN ORIGINAL")
-		print("^1THERE WILL/MAY BE PROBLEMS WITH THE SCRIPT IF THE NAME IS CHANGED")
-		print("^1CHANGE NAME TO: ^2WH_Redemrp_FortAssault^1")
-		print("^1=====================================^0")
-	end
-end)
------------------------------------------------------------------------------CREDITS--------------------------------------------------------------------------------------
-AddEventHandler('onResourceStart', function(resourceName)
-	if (GetCurrentResourceName() ~= resourceName) then
-		return
-	end
-	print('^5 '..resourceName..'^2 Successfully Loaded ^7')
-    print(version )
-	print('^3 Maintained by WolfHouse Studios Dev Team ^7')
-	print('^1 If you require support join:^5 https://discord.gg/zGVDppYxfk ^7')
-    print('^3 https://digitalresources.tebex.io ^7')
-end)
---
